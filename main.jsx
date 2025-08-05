43dfec19283de76ee9d8ec00d067981690ab0c9d
agenthinsch <thinschb@alumni.uoguelph.ca>
2025-08-05 16:25:06 -0400

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import SurvivorVotingApp from "./SurvivorVotingApp";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <SurvivorVotingApp />
+  </React.StrictMode>
+);
